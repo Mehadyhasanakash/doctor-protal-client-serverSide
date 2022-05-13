b45ca93c9cbc1b56ae80f0b8ad6a472f90914872
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-05-13 11:18:55 +0600

    frist-commt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
+require('dotenv').config()
 
 app.use(cors());
 
@@ -11,9 +12,38 @@ app.use(express.json());
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aesb1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
+async function run() {
+
+try{
+    await client.connect();
+
+    console.log('db_contact')
+
+    const doctorBooking = client.db("doctor_protal").collection('services')
+
+   
+   
+    app.get('/service', async(req, res) =>{
+
+        const query ={}
+        const cursor = doctorBooking.find(query);
+        const services = await cursor.toArray();
+        res.send(services)
+       
+    })
+
+}
+  finally {
+    
+  }
+}
+run().catch(console.dir);
 
 
 
